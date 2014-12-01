82a8d159f6a1e93e93b25d744c36aff6b950f668
William Huang <whuang6@stanford.edu>
2014-12-01 05:16:19 -0800

    create works

---
+++ Mix/CreateView.h
@@ -0,0 +1,13 @@
+//
+//  CreateView.h
+//  Mix
+//
+//  Created by William Huang on 12/1/14.
+//  Copyright (c) 2014 Kirby Gee - Stanford Univeristy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateView : UIView
+
+@end
